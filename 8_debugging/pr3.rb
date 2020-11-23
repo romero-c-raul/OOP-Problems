c077c91b7d9ed887e2729eac1ee0a632e2eed0af
Raul Romero <raulromerocano@aggienetwork.com>
2020-11-23 18:13:21 +0000

    Completed Debuggining problems

---
+++ 8_debugging/pr3.rb
@@ -0,0 +1,52 @@
+=begin
+- On lines 37 and 38 of our code, we can see that grace and ada are located at the
+  same coordinates. So why does line 39 output false? Fix the code to produce the 
+  expected output.
+=end
+
+
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+end
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+  
+  def ==(other_object)
+    latitude == other_object.latitude &&
+    longitude == other_object.longitude
+  end
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location # expected: true
+                                    # actual: false
